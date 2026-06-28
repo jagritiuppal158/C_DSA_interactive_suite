133b6baf57df8f7ea665c27b4ed02742c126945a
Pratikk404 <kalambepratik29@gmail.com>
2026-06-28 13:17:45 +0000

    feat(string-algos): clear screen and show a header before each sub-demo

--- src/string_algorithms/string_algorithms_demo.c
+++ src/string_algorithms/string_algorithms_demo.c
@@ -1,3 +1,4 @@
+#include "display_header.h"
 #include "safe_input.h"
 #include "string_algorithms.h"
 #include <stdio.h>
@@ -26,12 +27,15 @@ void string_algorithms_demo(void)
         switch (choice)
         {
             case 1:
+                display_header("Naive String Matching");
                 naive_string_matching_demo();
                 break;
             case 2:
+                display_header("Knuth-Morris-Pratt (KMP)");
                 kmp_demo();
                 break;
             case 3:
+                display_header("Rabin-Karp");
                 rabin_karp_demo();
                 break;
         }
